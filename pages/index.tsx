dc0e6c53da7aba24f4d51f2ffa1bd2945ad4e7d6
Ryan Donohue <ryan@rdonohue.ca>
2021-02-20 17:46:51 -0600

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
-import { Flex, Img, Text, VStack } from "@chakra-ui/react";
+import {
+  Flex,
+  Img,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Head from "next/head";
+import React from "react";
 import ContactForm from "./components/ContactForm";
 import InfoBox from "./components/InfoBox";
 
@@ -48,68 +59,80 @@ export default function Home() {
         />
       </Flex>
 
-      <VStack spacing={10}>
-        <Text
-          fontSize="3rem"
-          lineHeight={1.15}
-          margin={0}
-          textAlign="center"
-          fontWeight={500}
-        >
-          Bout Me
-        </Text>
+      <Tabs isFitted variant="enclosed">
+        <TabList>
+          <Tab>Work</Tab>
+          <Tab>Personal</Tab>
+        </TabList>
 
-        <InfoBox
-          title="Who Am I?"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
-          imagePath="./assets/whoamicropped.jpg"
-        />
+        <TabPanels>
+          <TabPanel>
+            <VStack spacing={10}>
+              <Text
+                fontSize="3rem"
+                lineHeight={1.15}
+                margin={0}
+                textAlign="center"
+                fontWeight={500}
+              >
+                Bout Work
+              </Text>
 
-        <InfoBox
-          title="What Do I Do?"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
-          imagePath="./assets/me-n-pat.jpg"
-          reverse
-        />
-      </VStack>
+              <InfoBox
+                title="54e Dev Studios"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
+                imagePath="./assets/54e.webp"
+              />
 
-      <VStack spacing={10}>
-        <Text
-          fontSize="3rem"
-          lineHeight={1.15}
-          margin={0}
-          textAlign="center"
-          fontWeight={500}
-        >
-          Bout Work
-        </Text>
+              <InfoBox
+                title="Outpost Health"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
+                imagePath="./assets/outpost.svg"
+                reverse
+              />
 
-        <InfoBox
-          title="54e Dev Studios"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
-          imagePath="./assets/54e.webp"
-        />
+              <InfoBox
+                title="Brew Ninja"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
+                imagePath="./assets/brewninja.png"
+              />
 
-        <InfoBox
-          title="Outpost Health"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
-          imagePath="./assets/outpost.svg"
-          reverse
-        />
+              <InfoBox
+                title="Vivvo"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
+                imagePath="./assets/vivvo.svg"
+                reverse
+              />
+            </VStack>
+          </TabPanel>
+          <TabPanel>
+            <VStack spacing={10}>
+              <Text
+                fontSize="3rem"
+                lineHeight={1.15}
+                margin={0}
+                textAlign="center"
+                fontWeight={500}
+              >
+                Bout Me
+              </Text>
 
-        <InfoBox
-          title="Brew Ninja"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
-          imagePath="./assets/brewninja.png"
-        />
+              <InfoBox
+                title="Who Am I?"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
+                imagePath="./assets/whoamicropped.jpg"
+              />
 
-        <InfoBox
-          title="Vivvo"
-          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
-          imagePath="./assets/vivvo.svg"
-          reverse
-        />
-      </VStack>
+              <InfoBox
+                title="What Do I Do?"
+                body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum cursus augue, et fermentum elit malesuada eget. Maecenas vehicula lacus eu ex finibus congue."
+                imagePath="./assets/me-n-pat.jpg"
+                reverse
+              />
+            </VStack>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
 
       <ContactForm />
 
