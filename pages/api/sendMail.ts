d5faee738e50aaad79a35e69c544d06dfded9570
Ryan <ryan@rdonohue.ca>
2021-02-21 10:56:21 -0600

    init to mail

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -1,5 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+
+  const mailgun = require("mailgun-js");
+  const DOMAIN = process.env.MAILGUN_BASE_URL;
+  const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN });
+  const data = {
+    from: "Excited User <me@samples.mailgun.org>",
+    to: "ryan@rdonohue.ca",
+    subject: "Hello",
+    text: "Testing some Mailgun awesomness!",
+  };
+  mg.messages().send(data, function (error, body) {
+    console.log(body);
+    console.log(error)
+  });
+
+  res.status(200).json({});
+
+};
